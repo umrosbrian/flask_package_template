ab309828a9162281bfefd2b7b1a9ad2756580969
rosbrian <rosbrian@umich.edu>
2022-11-07 15:41:59 -0500

    added templates directory

--- src/pkg/routes.py
+++ src/pkg/routes.py
@@ -1,5 +1,7 @@
 from pkg import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Brian'}
+    return render_template('index.html', title='Home', user=user)
